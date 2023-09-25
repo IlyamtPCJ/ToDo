c8fdc69565e7d98d0165107b15dbc2f00521b159
IlyamtPCJ <144455673+IlyamtPCJ@users.noreply.github.com>
2023-09-26 02:33:59 +0500

    Add files via upload

---
+++ JS + html/script.js
@@ -0,0 +1,32 @@
+const inputEl = document.getElementById('input')
+const btnEl = document.getElementsByClassName('.todo-list_btn')
+const fieldEl = document.querySelector('.todo-list_field')
+
+function onBtnClick() {
+    if (inputEl.value.length) {
+     const divEl = document.createElement('div')
+     divEl.classList.add('todo-list_item')
+
+     const checkbox = document.createElement('input')
+     checkbox.type = 'checkbox'
+    
+     const text = document.createElement('p')
+     text.classList.add(todo-list_item-text)
+     text.innerText = inputEl.value
+
+     const img = document.createElement('img')
+     img.src = 'trash.svg'
+
+     divEl.appendChild(checkbox)
+     divEl.appendChild(text)
+     divEl.appendChild(img)
+
+
+     fieldEl.appendChild(divEl)
+     inputEl.value = ''
+    }
+}
+
+btnEl.addEventListener('click', () => {
+    console.log('Clicked!');
+})
