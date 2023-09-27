b5e8790c4fc7d2c70e0c2b4f6007d53247afaa5a
IlyamtPCJ <144455673+IlyamtPCJ@users.noreply.github.com>
2023-09-27 13:25:52 +0500

    Add files via upload

--- JS + html/script.js
+++ JS + html/script.js
@@ -1,32 +1,34 @@
-const inputEl = document.getElementById('input')
+const inputEl = document.getElementById('example')
 const btnEl = document.getElementsByClassName('.todo-list_btn')
 const fieldEl = document.querySelector('.todo-list_field')
 
 function onBtnClick() {
-    if (inputEl.value.length) {
-     const divEl = document.createElement('div')
-     divEl.classList.add('todo-list_item')
+    if (inputEl.value.length){
+        const divEl = document.createElement('div')
+        divEl.classList.add('todo-list_item')
 
-     const checkbox = document.createElement('input')
-     checkbox.type = 'checkbox'
+        const checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
     
-     const text = document.createElement('p')
-     text.classList.add(todo-list_item-text)
-     text.innerText = inputEl.value
+        const text = document.createElement('p')
+        text.classList.add(todo-list_item-text)
+        text.innerText = inputEl.value
 
-     const img = document.createElement('img')
-     img.src = 'trash.svg'
+        const img = document.createElement('img')
+        img.src = 'trash.svg'
 
-     divEl.appendChild(checkbox)
-     divEl.appendChild(text)
-     divEl.appendChild(img)
+        divEl.appendChild(checkbox)
+        divEl.appendChild(text)
+        divEl.appendChild(img)
 
-
-     fieldEl.appendChild(divEl)
-     inputEl.value = ''
+        fieldEl.appendChild(divEl)
+        inputEl.value = ''
     }
 }
 
-btnEl.addEventListener('click', () => {
+btnEl.addEventListener('click', onBtnClick)
     console.log('Clicked!');
-})
+
+//btnEl.addEventListener('click', () => {
+//    console.log('Clicked!');
+//})
