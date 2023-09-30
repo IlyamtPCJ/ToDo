cbb2094bb01abcb95df50b407e6111cec1f2d194
IlyamtPCJ <144455673+IlyamtPCJ@users.noreply.github.com>
2023-09-30 14:16:11 +0500

    Add files via upload

--- JS + html/script.js
+++ JS + html/script.js
@@ -2,32 +2,93 @@ const inputEl = document.getElementById('input')
 const btnEl = document.querySelector('.todo-list_btn')
 const fieldEl = document.querySelector('.todo-list_field')
 
-function onBtnClick() {
-    if (inputEl.value.length){
+const todoList = [
+    {
+        id: 1,
+        text: 'Some text',
+        isDone: false
+    },
+    {
+        id: 2,
+        text: 'Some text',
+        isDone: true
+    }
+]
+
+let id = 3
+
+function render() {
+    fieldEl.innerHTML = ''
+    for(let item of todoList) {
+        const el = createHtmlElement(item)
+        fieldEl.appendChild(el)
+    }
+}
+
+function createHtmlElement(item) {
         const divEl = document.createElement('div')
         divEl.classList.add('todo-list_item')
 
         const checkbox = document.createElement('input')
         checkbox.type = 'checkbox'
+        if(item.isDone) {
+            checkbox.checked = true
+            divEl.classList.add('todo-list_item_done')
+        }
     
         const text = document.createElement('p')
         text.classList.add('todo-list_item_text')
-        text.innerText = inputEl.value
+        text.innerText = item.text
 
         const img = document.createElement('img')
         img.src = 'trash.svg'
+
+        img.addEventListener('click', ()=>{
+            removeItem(item.id)
+        })
 
         divEl.appendChild(checkbox)
         divEl.appendChild(text)
         divEl.appendChild(img)
 
-        fieldEl.appendChild(divEl)
+        checkbox.addEventListener('change', ()=>{
+            updateItem(item.id)
+        })
+
+        return divEl
+    }
+
+    function updateItem(id) {
+        const item = todoList.find((i) => i.id === id)
+        item.isDone = !item.isDone
+        render()
+    }
+
+    function removeItem(id) {
+        const idx = todoList.findIndex(i => i.id === id)
+        todoList.splice(idx, 1)
+        render()
+    }
+
+function addItem() {
+    if(inputEl.value) {
+        const obj = {
+            text: inputEl.value,
+            isDone: false,
+            id: id++
+        }
+        //obj.text = inputEl.value
+        //obj.isDone = false
+        //obj.id = id++       (id = id + 1) (id += 1 or 2 or 3 ...)
+        todoList.push(obj)
         inputEl.value = ''
+        render()
     }
 }
 
-btnEl.addEventListener('click', onBtnClick)
-    console.log('Clicked!');
+btnEl.addEventListener('click', addItem)
+
+render()
 
 //btnEl.addEventListener('click', () => {
 //    console.log('Clicked!');
